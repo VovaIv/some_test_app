ae7594b6d7f51465ec185e9005b3b50e9a6db95b
Volodymyr Horodchuk <vovan_tl@ukr.net>
2024-12-23 20:19:04 -0600

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,6 +94,5 @@ RSpec.configure do |config|
   config.before(:each) do
     redis_instance = MockRedis.new
     allow(Redis).to receive(:new).and_return(redis_instance)
-    allow(Redis::Store).to receive(:new).and_return(redis_instance)
   end
 end
